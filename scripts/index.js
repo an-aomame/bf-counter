f2101c7b8488b652af708c6099a5c427a49e6630
aomame <an.aomame@gmail.com>
2016-06-24 07:29:00 +0900

    NodeList が iterable でないブラウザでエラーが出ていたのを修正

--- scripts/index.js
+++ scripts/index.js
@@ -4,5 +4,8 @@ require('../stylesheets/style.css');
 require('viewport-units-buggyfill').init();
 require('fastclick').attach(document.body);
 
+require('core-js/web/dom-collections');
+require('core-js/fn/dom-collections/iterator');
+
 require('./scroll.js').default();
 require('./counter.js').default();
