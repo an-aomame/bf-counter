7a6e4de2b3281f516a1134bef7ef308fba5c430e
aomame <an.aomame@gmail.com>
2016-06-24 00:13:58 +0900

    html 版を移植

--- entry.js
+++ entry.js
@@ -1,2 +1,40 @@
-require('./stylesheets/sample.css');
-document.write('entry.js');
+require('./stylesheets/style.css');
+
+window.viewportUnitsBuggyfill.init();
+
+jQuery(function($) {
+  // control scrolling
+  (function() {
+    var scrollTop = function() {
+      document.body.scrollTop = document.documentElement.scrollTop = 0;
+    };
+    var $contents = $('.side1 > .contents');
+    scrollTop();
+    $('body').on('touchmove', function(e) {
+      if (window.innerHeight < $contents.height()) return;
+      e.preventDefault();
+      scrollTop();
+    });
+  })();
+
+  $('.counter').each(function() {
+    var $counter = $(this);
+    var $count = $counter.find('.count');
+    var defaultCnt = Number($count.text());
+    var cnt = defaultCnt;
+    function updateCnt(newCnt) {
+      cnt = newCnt;
+      $count.text(cnt);
+    }
+    $counter.find('.diff-btn').each(function() {
+      var $btn = $(this);
+      var diff = Number($btn.data('diff'));
+      $btn.click(function() {
+        updateCnt(cnt + diff);
+      });
+    });
+    $counter.find('.reset-btn').click(function() {
+      updateCnt(defaultCnt);
+    });
+  });
+});
